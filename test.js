09dc37cae731f17afcae969c7cb9b80c3b8f645a
Dheerajp23 <59532632+Dheerajp23@users.noreply.github.com>
2025-01-10 05:46:23 +0530

    changes made to all files

--- test.js
+++ test.js
@@ -1,25 +1,65 @@
-function appendToDisplay(value){
-    document.querySelector("#display").value += value;
+const display = document.getElementById("display");
+const span= document.querySelector(".opert");
+let fn = 0;
+let sn = null;
+let tn = 0;
+
+function setNumber(e){
+    console.log(e);
+   
+    display.value += e;
+    fn = display.value;
+}
+
+function operator(e){
+    tn = fn;
+    sn = e;
+    fn = "";
+    display.value = "";
+    span.innerText = e;
+}
+function Calculate(){
+    let a = Number(fn)
+    let b = Number(tn) 
+    let operator = sn;
+    let c = null;
+    switch (operator) {
+        case "+" : c =  a + b ;
+                    
+            break;
+        case "-" : c = b - a;
+        break;
+        case "*" : c = b * a;
+                        
+                        
+        break;
+        case "/" : if(a === 0){
+            c = "error";
+        }
+        else{
+            c = b / a}
+        break;
+
+     
+        default:
+            break;
+    }
+   
+    fn = c
+    tn = null;
+   
+    display.value =  c;
+}
+
+function singleClear(){
+    const a = display.value;
+    const b = a.slice(0 ,-1)
+    display.value = b;
 }
 function clearAll(){
-    document.querySelector("#display").value = "";
-}
-
-function Calculate(){
-   let value = document.querySelector("#display").value;
-    if (value === "") {
-        document.querySelector("#display").value = "";
-   } else {
-    try {
-        let result = eval(document.querySelector("#display").value);
-        document.querySelector("#display").value= result;
-    } catch (error) {
-        document.querySelector("#display").value="Error"
-    }
-   }
-}
-
-function clearOne(){
-    let string = document.querySelector("#display").value.slice(0 , -1);
-    document.querySelector("#display").value = string;
+    display.value = "";
+    span.innerText = "";
+    fn = 0;
+    sn = null;
+    tn = 0;
 }
